aa4a7e2d7b7e17b4924e7fb3f616bf592e67a33b
wsmanchan <wsmanchan@cps.edu>
2025-01-10 08:36:38 -0600

    Fantasy Name
    This gives the player to answer a total of three questions leading them to a fun fantasy name.

---
+++ Fantasy.py
@@ -0,0 +1,39 @@
+#Stacie Chan
+#10/17/2024
+#Fantasy Name Project
+
+#Main
+print("Welcome to your fantasy name")
+print("Answer all the questions to find out your fantasy name")
+print("Please answer the questions in lowercase")
+ans = input("fairy or mermaid?")
+if ans == "fairy":
+    ans = input("light or dark?")
+    if ans == "dark":
+        ans = input("hot or cold?")
+        if ans == "hot":
+             print("Your fantasy name is Alyssa.")
+        else:
+             print("Your fantasy name is Luna.")
+    if ans == "light":
+        ans = input("berry or citrus?")
+        if ans == "berry":
+            print("Your fantasy name is Sophie.")
+        else:
+            print("Your fantasy name is Claire.")
+if ans == "mermaid":
+    ans = input("dolphin or shark?")
+    if ans == "dolphin":
+        ans = input ("phone or computer?")
+        if ans == "phone":
+            print("Your fantasy name is Daphne.")
+        else:
+            print("Your fantasy name is Elena.")
+    if ans == "shark":
+        ans = input("vintage or modern?")
+        if ans == "vintage":
+            print("Your fantasy name is Kira.")
+        else:
+            print("Your fantasy name is Irena.")
+
+
